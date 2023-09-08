6e1923108679eb3ca2a1f8bb8ac4783f230e6e89
jahlinny <139661626+jahlinny@users.noreply.github.com>
2023-09-08 17:47:20 -0400

    Add files via upload
    simple repeating calculator ( can continuously add operations until you type NO to stop )

---
+++ calc.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+func div(x, y float64) (z float64) {
+	z = x / y
+	return
+}
+
+func mult(x, y float64) (z float64) {
+	z = x * y
+	return
+}
+func add(x, y float64) (z float64) {
+	z = x + y
+	return
+}
+func sub(x, y float64) (z float64) {
+	z = x - y
+	return
+}
+
+func main() {
+	var num float64
+	fmt.Println("Type the number you want to perform your operations on")
+	fmt.Scan(&num)
+	for {
+		var sign string
+		var calc float64
+
+		fmt.Println("Type the operand symbol (+, -, *, /) and the number you want to operate the input on seperate lines ( ex: / (enter) 9 (enter) = /9 ): ")
+		fmt.Scan(&sign, &calc)
+
+		switch sign {
+		case "/":
+			num = div(num, calc)
+		case "*":
+			num = mult(num, calc)
+
+		case "+":
+			num = add(num, calc)
+		case "-":
+			num = sub(num, calc)
+		default:
+			fmt.Println("Sign unavailable, please try again with /, *, +, -")
+			continue
+		}
+		fmt.Println("Current Total: ", num)
+		var confirm string
+		fmt.Printf("To continue your operation, type YES, to break out of the program, type NO: ")
+		fmt.Scan(&confirm)
+
+		if confirm == "NO" {
+			fmt.Println("Goodbye")
+			break
+		}
+
+		/*if strings.Contains(sign, "/") {
+			fmt.Println("Answer: ", div(num, calc))
+		} */
+
+	}
+}
