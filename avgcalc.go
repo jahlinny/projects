4a0dbd4eb556bb6e79a1c07eee0825ee2adb615d
jahlinny <139661626+jahlinny@users.noreply.github.com>
2023-09-14 19:52:29 -0400

    Update avgcalc.go
    updated the average calculator to be a bit easier and look a bit better

--- avgcalc.go
+++ avgcalc.go
@@ -4,42 +4,28 @@ import (
 	"fmt"
 )
 
-func add(x, y float64) (z float64) {
-	z = x + y
-	return
-}
-
 func main() {
-	var num float64
-	fmt.Print("Type in your starting number: ")
-	fmt.Scan(&num)
-	count := 1
-	for {
-		var conf string
-		fmt.Println("Type YES to continue adding numbers to your average or type DONE to find your average based off of your number inputs")
-		fmt.Scan(&conf)
 
-		if conf == "YES" {
-			var num1 float64
-			fmt.Print("Type in the number you want to add: ")
-			fmt.Scan(&num1)
-			num = add(num, num1)
-			fmt.Println("Total: ", num)
-			count++
-			continue
-		} else if conf != "DONE" {
-			fmt.Println("Please enter DONE to calculate the average or YES to keep adding values")
-			continue
-		} else if conf == "DONE" {
-			if count == 1 {
-				fmt.Println("No numbers added")
-				break
-			} else if count > 1 {
+	var top int
+	fmt.Println("How many times do you want to append")
+	fmt.Scan(&top)
 
-				fmt.Println("Average: ", num/float64(count))
-				break
-			}
-		}
+	arr := []int{}
+
+	for i := 1; i <= top; i++ {
+
+		var app int
+		fmt.Println("Type the values you want to append into the slice")
+		fmt.Scan(&app)
+		arr = append(arr, app)
+		fmt.Println(arr)
+	}
+	sum := 0
+	for _, num := range arr {
+		sum += num
 
 	}
+	tot := float64(sum) / float64(top)
+	fmt.Printf("Average: %v \n", tot)
+	fmt.Printf("Total Value of integers: %v \n", sum)
 }
